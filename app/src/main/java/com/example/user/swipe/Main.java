94d9c864e3b107fb4c72ed9c34f44f119649162e
Alex <Alex>
2016-03-25 22:26:39 +0300

    Initial commit

---
+++ app/src/main/java/com/example/user/swipe/Main.java
@@ -0,0 +1,22 @@
+package com.example.user.swipe;
+
+import android.os.Bundle;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+public class Main extends AppCompatActivity {
+
+    static final String TAG = "myLogs";
+    static final int PAGE_COUNT = 10;
+
+    ViewPager pager;
+    PagerAdapter pagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+
+}
